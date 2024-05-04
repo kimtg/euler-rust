a64e48aa6e8420cd0e5313a65830773acddc9f02
KIM Taegyoon <steloflute@gmail.com>
2024-05-04 22:46:46 +0900

    format

--- euler9.rs
+++ euler9.rs
@@ -1,9 +1,9 @@
 fn main() {
     for a in 1..=999 {
-        for b in a+1..=999 {
+        for b in a + 1..=999 {
             let c = 1000 - a - b;
-            if c > 0 && a*a + b*b == c*c {
-                println!("{}", a*b*c);
+            if c > 0 && a * a + b * b == c * c {
+                println!("{}", a * b * c);
                 return;
             }
         }
